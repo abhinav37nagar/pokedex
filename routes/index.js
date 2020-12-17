879ef14205a50d6576099558158c1ba16b1bcb66
Abhinav Nagar <abhinav37nagar@gmail.com>
2020-12-17 17:38:51 +0530

    -added css -added login with google -added logout -added middleware for login/logout

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 // @desc    Landing Page
 // @route   GET /
-router.get("/", (req, res) => {
-  res.render("login");
+router.get("/", ensureGuest, (req, res) => {
+  res.render("login", { layout: "login" });
 });
 
 // @desc    Dashboard
 // @route   GET /dex
-router.get("/dex", (req, res) => {
+router.get("/dex", ensureAuth, (req, res) => {
+  console.log(req.user);
   res.render("dex");
 });
 
